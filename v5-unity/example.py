16d64f18028162624a3d50bf598c6503c7a67d38
ANUSHMANGAL01 <anushmangal01@gmail.com>
2021-11-15 21:19:15 +0530

    changed background

--- v5-unity/example.py
+++ v5-unity/example.py
@@ -1,43 +1,42 @@
-# class Anush:
-#     t= 4.5
-#     f = False
-#     def hell():
-#         print("Hell")
-#     def hello():
-#         print("Hello")
-# print(10+5)
-# ffff = True
-# for i in range(5):
-#     print(i)
-def product(a,b):
-    return a*b
+class Anush:
+    t= 4.5
+    f = False
+    def hell():
+        print("Hell")
+    def hello():
+        print("Hello")
+print(10+5)
+ffff = True
+for i in range(5):
+    print(i)
+
 def fact(n):
     if n==0:
         return 1
     else:
-        return product(n,fact(n-1))
+        return n * fact(n-1);
 print(fact(2))
-# list = [1,2,True, "Hello", 4.5]
-# list2 = [3,3,3,3]
+list = [1,2,True, "Hello", 4.5]
+list2 = [3,3,3,3]
 
-# for i in range(2):
-#     j=0
-#     while(j<1):
-#         print(j)
-#         j=j+1
-# i=0
-# def hello():
-#     t="Hi"
-#     print("Hello")
-# hello()
-# while(i<2):
-#     j=0
-#     while(j<2):
-#         hello()
-#         j=j+2
-#     i=i+1
+for i in range(2):
+    j=0
+    while(j<1):
+        print(j)
+        j=j+1
+i=0
+def hello():
+    t="Hi"
+    print("Hello")
+hello()
+while(i<2):
+    j=0
+    while(j<2):
+        hello()
+        j=j+2
+    i=i+1
 
-# class Anush1:
-#     def hello():
-#         print("Hello")
+class Anush1:
+    def hello():
+        print("Hello")
         
