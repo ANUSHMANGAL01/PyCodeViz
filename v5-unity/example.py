986732919d23ec1c3aa1f482c58e728922547aac
ANUSHMANGAL01 <anushmangal01@gmail.com>
2021-11-15 09:07:37 +0530

    Added example.py file

---
+++ v5-unity/example.py
@@ -0,0 +1,39 @@
+class Anush:
+    t= 4.5
+    f = False
+    def hell():
+        print("Hell")
+    def hello():
+        print("Hello")
+print(10+5)
+
+for i in range(5):
+    print(i)
+    
+def fact(n):
+    if n==0:
+        return 1
+    else:
+        return n*fact(n-1)
+print(fact(2))
+list = [1,2,True, "Hello", 4.5]
+
+for i in range(2):
+    j=0
+    while(j<1):
+        print(j)
+        j=j+1
+i=0
+def hello():
+    print("Hello")
+while(i<2):
+    j=0
+    while(j<2):
+        hello()
+        j=j+2
+    i=i+1
+
+class Anush1:
+    def hello():
+        print("Hello")
+        
